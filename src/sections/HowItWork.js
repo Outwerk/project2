edf216136add83192f228adb7bb4db6ee230f17d
Outwerk <outows50@gmail.com>
2024-10-18 06:36:10 -0700

    make responsive all sections

--- src/sections/HowItWork.js
+++ src/sections/HowItWork.js
@@ -32,7 +32,7 @@ const HowItWorks = () => {
     return (
         <>
             <div id="howItWork" className='h-full md:h-screen py-10 md:pt-28 '>
-                <h2 className="text-3xl font-bold mb-6 text-center underline-thick uppercase">How It Works</h2>
+                <h2 className="text-3xl font-bold mb-6 text-center underline-thick uppercase mt-6 md:mt-0">How It Works</h2>
 
                 <div className='md:ms-52 p-3 flex flex-col items-center md:items-start justify-center gap-8'>
 
